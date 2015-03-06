03b1cfc39d6399805e8bfbb59739103d308a46aa
robindevouge <robin.devouge@gmail.com>
2015-03-06 15:26:36 +0100

    doing great shit

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-
 	
+	
+	
+	$(window).scroll(function() {
+		
+		var navPos = $(".mainNav").offset().top,
+			pagePos = $(window).scrollTop();
+		console.log("scroll");
+
+		if (pagePos >= navPos) {
+		   $(".mainNav").addClass("onTop");
+			console.log("add");
+		} else if (pagePos < navPos) {
+			$(".mainNav").removeClass("onTop");
+			console.log("rmv");
+		}
+	});
+	
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html,body').animate({
+			  scrollTop: target.offset().top
+			}, 750);
+			return false;
+		  }
+		}
+	  });
+	});
 	
 });
